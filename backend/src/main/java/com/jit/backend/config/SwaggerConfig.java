6e42bcd5ffa59a478abbc2d65fc5441930d6c162
sbs1621 <qudtjr4218@gmail.com>
2023-03-20 18:00:44 +0900

    docs:swagger 인증기능 추가

--- backend/src/main/java/com/jit/backend/config/SwaggerConfig.java
+++ backend/src/main/java/com/jit/backend/config/SwaggerConfig.java
@@ -3,7 +3,8 @@ package com.jit.backend.config;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
-import org.springframework.beans.factory.annotation.Value;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -17,8 +18,21 @@ public class SwaggerConfig {
                 .title("JIT Factory API Document")
                 .version("v0.0.1")
                 .description("JIT Factory API 명세서");
+        String jwtSchemeName = "jwtAuth";
+        // API 요청헤더에 인증정보 포함
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList(jwtSchemeName);
+        // SecuritySchemes 등록
+        Components components = new Components()
+                .addSecuritySchemes(jwtSchemeName, new SecurityScheme()
+                        .name(jwtSchemeName)
+                        .type(SecurityScheme.Type.HTTP) // HTTP 방식
+                        .scheme("bearer")
+                        .bearerFormat("JWT")); // 토큰 형식을 지정하는 임의의
         return new OpenAPI()
-                .components(new Components())
+                .components(components)
+                .addSecurityItem(securityRequirement)
                 .info(info);
     }
+
+
 }
