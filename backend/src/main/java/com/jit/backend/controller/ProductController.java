05baaebec2f9dca748487c8326a3fb74bc407fae
sbs1621 <qudtjr4218@gmail.com>
2023-03-22 18:29:57 +0900

    feat:product 조회, 추가기능 구현

---
+++ backend/src/main/java/com/jit/backend/controller/ProductController.java
@@ -0,0 +1,39 @@
+package com.jit.backend.controller;
+
+import com.jit.backend.dto.ProductDto;
+import com.jit.backend.entity.Product;
+import com.jit.backend.jwt.AuthDto;
+import com.jit.backend.service.ProductService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/product")
+@RequiredArgsConstructor
+public class ProductController {
+
+    private final ProductService productService;
+
+    @GetMapping("/dashboard")
+    public @ResponseBody
+    ResponseEntity dashboard() {
+        List<Product> productList;
+        try{
+            productList = productService.allProducts();
+        }catch (IllegalStateException e) {
+            return new ResponseEntity(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }return new ResponseEntity(productList, HttpStatus.OK);
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<Void> addProduct(@RequestBody @Valid ProductDto productDto) {
+        productService.addProduct(productDto);
+        return new ResponseEntity(HttpStatus.OK);
+    }
+
+}
---
+++ backend/src/main/java/com/jit/backend/entity/Product.java
@@ -0,0 +1,46 @@
+package com.jit.backend.entity;
+
+import com.jit.backend.dto.ProductDto;
+import com.jit.backend.jwt.AuthDto;
+import com.jit.backend.role.Role;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "product")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder
+@AllArgsConstructor
+public class Product {
+
+    @Id
+    @Column(name = "productId")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String productName;
+
+    @Column
+    private String status;
+
+    @Column
+    private Integer sales;
+
+    @Column
+    private LocalDateTime createTime;
+
+    public static Product addProduct(ProductDto productDto) {
+        Product product = new Product();
+        product.productName = productDto.getProductName();
+        product.status = productDto.getStatus();
+        product.sales = productDto.getSales();
+        product.createTime = LocalDateTime.now();
+        return product;
+    }
+
+}
