9969aad7b573f375eb8a0594c611c4e615b6d409
sbs1621 <qudtjr4218@gmail.com>
2023-04-04 19:49:23 +0900

    feat:MySQL 도입

--- backend/src/main/java/com/jit/backend/service/OrdersService.java
+++ backend/src/main/java/com/jit/backend/service/OrdersService.java
@@ -14,7 +14,7 @@ import java.util.List;
 
 @Service
 @RequiredArgsConstructor
-@Transactional(readOnly = true)
+@Transactional
 public class OrdersService {
     private final OrdersRepository ordersRepository;
     public List<Orders> allOrder()  {
