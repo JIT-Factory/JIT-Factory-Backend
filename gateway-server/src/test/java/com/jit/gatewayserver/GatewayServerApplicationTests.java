cf4001e010738c9d36b392a95751a36feb0e2709
sbs1621 <qudtjr4218@gmail.com>
2023-06-20 16:26:24 +0900

    feat:Eureka Gateway 구성

---
+++ gateway-server/src/test/java/com/jit/gatewayserver/GatewayServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jit.gatewayserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
--- gateway/src/main/java/com/jit/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/jit/gateway/GatewayApplication.java
@@ -1,14 +1,15 @@
-package com.jit.backend;
+package com.jit.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
-public class BackendApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(BackendApplication.class, args);
-    }
+@EnableDiscoveryClient // 유레카 클라이언트 추가
+public class GatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayApplication.class, args);
+	}
 
 }
